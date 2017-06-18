0a0c3ed7fac33fd1f665a0a81bc21cc581d721a1
Or Ouziel <ouzielor@gmail.com>
2017-06-18 03:46:12 +0300

    removed unneeded package

--- src/user/auth/unRegisterUser/index.ts
+++ src/user/auth/unRegisterUser/index.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions'
 import {admin} from '../../../admin'
-const cors = require('cors')({origin: true});
 
 export const unRegisterUser = functions.auth.user().onDelete(event => {
   admin.database().ref('users/' + event.data.uid).set(null)
